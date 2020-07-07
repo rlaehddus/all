dfed987f0c01f8540f6a4331e9001464167e4120
hyojeong <khj6165@gmail.com>
2020-07-08 03:48:28 +0900

    저번수업자료 안가져온사람 손

---
+++ posts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin):    
+    list_display = (
+        'id',        
+        'title',        
+        'view_count',        
+        'created_at',    
+    )
+    search_fields = (
+        'title', 
+    )
